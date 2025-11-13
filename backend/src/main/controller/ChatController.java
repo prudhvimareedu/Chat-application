7675f2f17a93a96bf84168137773096331d0f3d9
prudhvimareedu <prudhvimareedu89@gmail.com>
2025-11-13 23:33:40 +0530

    Add files via upload

---
+++ backend/src/main/controller/ChatController.java
@@ -0,0 +1,16 @@
+package com.chatapp.controller;
+
+import com.chatapp.model.Message;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/topic/public")
+    public Message sendMessage(Message message) {
+        return message;
+    }
+}
